65b92b1d825f613a17f0b6238a2fb3bfe15f3d93
Lihan Li <frankieteardrop@gmail.com>
2013-01-14 14:10:04 -0500

    test switch to window method

--- test/tests/wist_test.rb
+++ test/tests/wist_test.rb
@@ -18,8 +18,25 @@ class TestWist < CapybaraTestCase
     wait_until { button.text == 'clicked' }
   end
 
-  def test_tweet_button
+  def test_verify_tweet_button
     verify_tweet_button 'check this out'
+  end
+
+  def test_switch_to_window_and_execute
+    assert_on_window_1 = -> { assert current_path.match(/test.html$/) }
+
+    assert_on_window_1.()
+    click '#switch_window_test'
+
+    switch_to_window_and_execute do
+      assert current_path.match(/test1.html$/)
+    end
+
+    assert_on_window_1.()
+  end
+
+  def test_get_js
+    assert get_js('document.URL').match(/test.html$/)
   end
 
 end
